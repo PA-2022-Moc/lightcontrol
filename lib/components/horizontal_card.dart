92434f33a81b71fa0ede69aa6cab7b49068d7bca
luc <luc.coujonde@gmail.com>
2022-04-17 20:30:44 +0200

    presque

---
+++ lib/components/horizontal_card.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class HorizontalCaroussel extends StatefulWidget {
+  final String content;
+  const HorizontalCaroussel({Key? key, required this.content})
+      : super(key: key);
+
+  @override
+  State<HorizontalCaroussel> createState() => _HorizontalCarousselState();
+}
+
+class _HorizontalCarousselState extends State<HorizontalCaroussel> {
+  int _index = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: SizedBox(
+          child: PageView.builder(
+            itemCount: 2,
+            controller: PageController(viewportFraction: 0.7),
+            onPageChanged: (int index) => setState(() => _index = index),
+            itemBuilder: (_, i) {
+              return Transform.scale(
+                scale: i == _index ? 1 : 0.9,
+                child: Card(
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Center(
+                    child: CardContent(_index, widget.content),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget CardContent(int index, String content) {
+    if (index == 0) {
+      return Text(
+        "${content}",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 20,
+          height: 1.5,
+        ),
+      );
+    } else {
+      return Text(
+        "Card ${index}",
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 20, height: 1.5),
+      );
+    }
+  }
+}
--- lib/views/my_home_page.dart
+++ lib/views/my_home_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:lightcontrol/components/brightness_cursor.dart';
 import 'package:lightcontrol/components/color_palette.dart';
+import 'package:lightcontrol/components/horizontal_card.dart';
 import 'package:lightcontrol/components/switch_button.dart';
 import 'package:lightcontrol/lamp.dart';
 
 class MyHomePage extends StatefulWidget {
   String infoLamp;
   Lamp lamp;
-  MyHomePage({Key? key, required this.infoLamp, required this.lamp}) : super(key: key);
+  MyHomePage({Key? key, required this.infoLamp, required this.lamp})
+      : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -17,7 +19,17 @@ class _MyHomePageState extends State<MyHomePage> {
   bool isSwitched = false;
   bool isSwitched2 = false;
   double valueCursor = 50.0; // 50,0
-  var colorHex = {Colors.red: 'FF0000', Colors.orange: 'FFC000', Colors.yellow: 'FFFF00', Colors.white: 'FFFFFF', Colors.green: '00B050', Colors.lightBlue: '00B0F0', Colors.indigo.shade900: '002060', Colors.purple: '7030A0', Colors.pink.shade400: 'FD6C9E'};
+  var colorHex = {
+    Colors.red: 'FF0000',
+    Colors.orange: 'FFC000',
+    Colors.yellow: 'FFFF00',
+    Colors.white: 'FFFFFF',
+    Colors.green: '00B050',
+    Colors.lightBlue: '00B0F0',
+    Colors.indigo.shade900: '002060',
+    Colors.purple: '7030A0',
+    Colors.pink.shade400: 'FD6C9E'
+  };
 
   void selectColor(Color color) {
     if (!widget.lamp.checkIfOnOff()) {
@@ -85,22 +97,13 @@ class _MyHomePageState extends State<MyHomePage> {
         children: <Widget>[
           FractionallySizedBox(
             alignment: Alignment.topCenter,
-            widthFactor: 0.8,
+            widthFactor: 2.0,
             child: Container(
-              color: Colors.white,
-              height: 270,
-              margin: const EdgeInsets.only(top: 50.0),
-              padding: const EdgeInsets.all(10.0),
-              child: Text(
-                widget.infoLamp,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  // fontFamily: fontFamily,
-                  height: 1.5,
-                ),
-              ),
-            ),
+                color: Colors.white,
+                height: 280,
+                margin: const EdgeInsets.only(top: 0.8),
+                padding: const EdgeInsets.all(0.8),
+                child: HorizontalCaroussel(content: widget.infoLamp)),
           ),
           Expanded(
             // les color buttons
@@ -114,37 +117,64 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   Spacer(flex: 1),
-                  ColorPalette(setColor: () => setColor(Colors.red), color: Colors.red),
+                  ColorPalette(
+                      setColor: () => setColor(Colors.red), color: Colors.red),
                   Spacer(flex: 1),
-                  ColorPalette(setColor: () => setColor(Colors.orange), color: Colors.orange),
+                  ColorPalette(
+                      setColor: () => setColor(Colors.orange),
+                      color: Colors.orange),
                   Spacer(flex: 1),
-                  ColorPalette(setColor: () => setColor(Colors.yellow), color: Colors.yellow),
+                  ColorPalette(
+                      setColor: () => setColor(Colors.yellow),
+                      color: Colors.yellow),
                   Spacer(flex: 1),
-                  ColorPalette(setColor: () => setColor(Colors.white), color: Colors.white),
+                  ColorPalette(
+                      setColor: () => setColor(Colors.white),
+                      color: Colors.white),
                   Spacer(flex: 1),
-                  ColorPalette(setColor: () => setColor(Colors.green), color: Colors.green),
+                  ColorPalette(
+                      setColor: () => setColor(Colors.green),
+                      color: Colors.green),
                   Spacer(flex: 1),
-                  ColorPalette(setColor: () => setColor(Colors.lightBlue), color: Colors.lightBlue),
+                  ColorPalette(
+                      setColor: () => setColor(Colors.lightBlue),
+                      color: Colors.lightBlue),
                   Spacer(flex: 1),
-                  ColorPalette(setColor: () => setColor(Colors.indigo.shade900), color: Colors.indigo.shade900),
+                  ColorPalette(
+                      setColor: () => setColor(Colors.indigo.shade900),
+                      color: Colors.indigo.shade900),
                   Spacer(flex: 1),
-                  ColorPalette(setColor: () => setColor(Colors.purple), color: Colors.purple),
+                  ColorPalette(
+                      setColor: () => setColor(Colors.purple),
+                      color: Colors.purple),
                   Spacer(flex: 1),
-                  ColorPalette(setColor: () => setColor(Colors.pink.shade400), color: Colors.pink.shade400),
+                  ColorPalette(
+                      setColor: () => setColor(Colors.pink.shade400),
+                      color: Colors.pink.shade400),
                   Spacer(flex: 1),
                 ],
               ),
             ),
             flex: 1,
           ),
-          BrightnessCursor(valueCursor: valueCursor, setBrightness: setBrightness),
+          BrightnessCursor(
+              valueCursor: valueCursor, setBrightness: setBrightness),
           //  "  🎵" les switch button
 
           Expanded(
             child: Container(
               color: Colors.white,
               child: Row(
-                children: <Widget>[SwitchButton(activeText: "  💡", isSwitched: isSwitched, setSwitch: setSwitch), SwitchButton(activeText: "  🎵", isSwitched: isSwitched2, setSwitch: setSwitch2)],
+                children: <Widget>[
+                  SwitchButton(
+                      activeText: "  💡",
+                      isSwitched: isSwitched,
+                      setSwitch: setSwitch),
+                  SwitchButton(
+                      activeText: "  🎵",
+                      isSwitched: isSwitched2,
+                      setSwitch: setSwitch2)
+                ],
               ),
             ),
           ),
